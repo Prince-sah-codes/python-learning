f6faa926293dc5fab16520622039ef9e6062c251
Prince-sah-codes <sahprince4554@gmail.com>
2026-02-05 00:06:06 +0545

    commited

---
+++ even or odd.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a number: "))
+if a == 0:
+    print(" Number is zero, please try another number")
+
+elif a%2 != 0:
+    print(f"{a} is an  odd number")
+
+elif a%2 ==0:
+    print(f"{a} is an even number ")   
+
+         
